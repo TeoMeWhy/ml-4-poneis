823717a731f3bdbf54a77e49cad627e4ec52d51c
Teo Calvo <teocalvo2@gmail.com>
2024-05-14 11:37:57 -0300

    Clones

--- dia02/clones.py
+++ dia02/clones.py
@@ -2,13 +2,67 @@
 import pandas as pd
 
 df = pd.read_parquet("../data/dados_clones.parquet")
-
+df
 # %%
 ## Como podemos descobrir onde está o problema?
 # <Estatística descritiva>
 
+df.groupby(["Status "])[['Estatura(cm)', 'Massa(em kilos)']].mean()
 
 # %%
-## Como podemos fazer a máquina aprender?
+df['Status_bool'] = df['Status '] == 'Apto'
+df
 
-# <Machine Learning>
+# %%
+df.groupby(["Distância Ombro a ombro"])['Status_bool'].mean()
+
+# %%
+df.groupby(["Tamanho do crânio"])['Status_bool'].mean()
+
+# %%
+df.groupby(["Tamanho dos pés"])['Status_bool'].mean()
+
+# %%
+df.groupby(["General Jedi encarregado"])['Status_bool'].mean()
+
+# %%
+
+features = [
+    "Estatura(cm)",
+    "Massa(em kilos)",
+    "Distância Ombro a ombro",
+    "Tamanho do crânio",
+    "Tamanho dos pés",
+]
+
+cat_features = ["Distância Ombro a ombro",
+                "Tamanho do crânio",
+                "Tamanho dos pés"]
+
+X = df[features]
+
+# %%
+
+# Transformação de categorias para Numérico
+from feature_engine import encoding
+onehot = encoding.OneHotEncoder(variables=cat_features)
+onehot.fit(X)
+X = onehot.transform(X)
+X
+
+# %%
+
+from sklearn import tree
+arvore = tree.DecisionTreeClassifier(max_depth=3)
+arvore.fit(X, df["Status "])
+
+# %%
+
+import matplotlib.pyplot as plt
+plt.figure(dpi=600)
+tree.plot_tree(arvore,
+               class_names=arvore.classes_,
+               feature_names=X.columns,
+               filled=True,
+               )
+# %%
